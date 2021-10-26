dc6d68a8836bb92a9af91ccacd0411436580c38b
Rayssa Vitória de Castro Gonçalves <79856364+rayssadecastro@users.noreply.github.com>
2021-10-26 09:41:21 -0300

    aula 1 javascript

---
+++ Javascript/entradaviateclado/calculadora.js
@@ -0,0 +1,30 @@
+var num1 = parseInt(prompt('Digite o num1'));
+var num2 = parseInt(prompt('Digite o num2'));
+
+var soma = num1+num2;
+var subtracao = num1-num2;
+var divisao = num1/num2;
+var multiplicacao = num1*num2;
+
+document.getElementById('num1').innerHTML = num1;
+document.getElementById('num2').innerHTML = num2;
+
+document.getElementById('soma').innerHTML = soma;
+
+document.getElementById('subtracao').innerHTML = subtracao;
+
+
+document.getElementById('multiplicacao').innerHTML = multiplicacao;
+
+document.getElementById('divisao').innerHTML = divisao;
+
+
+
+
+
+
+
+
+
+
+//console.log(soma);
---
+++ Javascript/variaveis/exemplo.js
@@ -0,0 +1,9 @@
+var texto = "Vamos fazer uma calculadora";
+var num1 = 25;
+var num2 = 26;
+var soma = num1+num2
+alert(soma);        
+console.log(num1);
+//document.write(texto);
+
+document.getElementById('teste').innerHTML = texto;
